a04965086ae82c7b7c95652d0ed51e65a436f04c
Zadat Olayinka <bundayy@outlook.com>
2021-03-20 17:10:32 +0100

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div>
+    <div className="py-4">
       <p className="text-black text-xl text-center"> Github Repo Comparision</p>
       <p className="text-gray-500 text-sm text-center">
         Take your favorite repositories head-to-head!

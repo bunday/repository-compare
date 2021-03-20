116800691f9e313feb9ac6674a577f1d1e23f9fb
Zadat Olayinka <bundayy@outlook.com>
2021-03-20 17:10:56 +0100

    stat card content

--- src/components/StatCard.js
+++ src/components/StatCard.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React from "react";
 
-export default function StatCard() {
-    return (
-        <div className="w-1/5">
-          <div className="flex justify-center items-center">
-            <span> VS </span>
-          </div>
-          <div className="flex flex-col pt-16">
-            <div className="text-center">0 - 1</div>
-            <div className="text-center">1 - 0</div>
-            <div className="text-center">0 - 1</div>
-            <div className="text-center">1 - 0</div>
-          </div>
-          <div className="flex flex-col pt-16">
-            <div className="text-center text-xl">2 - 2</div>
+export default function StatCard({ state }) {
+  const { result } = state;
+  let totalFirst = result ? result.filter((res) => res.first === 1).length : 0;
+  let totalSecond = result
+    ? result.filter((res) => res.second === 1).length
+    : 0;
+  const getRepositorySignal = () => {
+    if (!result) return;
+    return result.map((res, i) => {
+      return (
+        <div key={i} className="text-center">
+          {" "}
+          {res.first} - {res.second}
+        </div>
+      );
+    });
+  };
+  return (
+    <div className="w-1/5">
+      <div className="flex justify-center items-center">
+        <span> VS </span>
+      </div>
+      <div className="flex flex-col pt-16">{getRepositorySignal()}</div>
+      {result && (
+        <div className="flex flex-col pt-16">
+          <div className="text-center text-xl">
+            {totalFirst} - {totalSecond}
           </div>
         </div>
-    )
+      )}
+    </div>
+  );
 }

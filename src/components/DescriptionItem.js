ffbd1d1a3458d813dbf5899301cdc00ed8fbfb56
Zadat Olayinka <bundayy@outlook.com>
2021-03-20 17:10:26 +0100

    dynamic description item

--- src/components/DescriptionItem.js
+++ src/components/DescriptionItem.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function DescriptionItem() {
+export default function DescriptionItem({title, value, icon}) {
   return (
     <div className="flex border-b justify-end py-2">
-      <span className="pr-4 text-gray-300 uppercase"> stars </span>
-      <span className="pr-4"> 154,900</span>
-      <img width="20" src="../icons/star.svg" />
+      <span className="pr-4 text-gray-300 uppercase"> {title} </span>
+      <span className="pr-4"> {value}</span>
+      <img width="20" src={icon} />
     </div>
   );
 }
